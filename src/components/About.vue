61a882154ddc69fc2228ca389aadc2bc9aaf1cbf
josermca01 <josermca01@gmail.com>
2024-11-11 17:04:03 -0300

    finish About tab

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,67 +1,103 @@
 <template>
-    <v-col cols="auto" class="d-flex flex-column pa-9">
-        <div class="fill-width d-flex text-high-emphasis text-h5">campo Sobre</div>
-        <div class="fill-width d-flex flex-column mb-2 text-medium-emphasis text-h5">
-            Tecnologias
-            <div class="fill-width d-flex flex-wrap text-subtitle-1">
-                <div v-for="tech in techs" :key="tech.name" class="bg-background3 d-flex align-center justify-center mr-3 mt-2 px-2 rounded"> 
-                    <v-avatar size="0.7rem" class=" mr-1" :id="tech.color">
-                    </v-avatar> {{ tech.name }}</div>
-            </div>
+  <v-col cols="auto" class="d-flex flex-column pa-9">
+    <div class="fill-width d-flex flex-column text-high-emphasis text-h5">Olá, eu sou um Desenvolvedor de Software
+      formado em Engenharia da Computação pela Facens de Sorocaba, com experiência em Desenvolvimento Web FullStack.
+      Atualmente estou estudando sobre VueJS, Angular e React no front-end e Springboot, Laravel e Flask no back-end.
+      Também possuo experiência com testes unitários usando JUnit e metodologias ágeis, como Kanban e Scrum. Estou
+      sempre buscando aprender novas tecnologias e aprimorar minhas habilidades para contribuir para o sucesso do
+      projeto e da equipe!
+    </div>
+    <br>
+    <div class="fill-width d-flex flex-column mb-2 text-medium-emphasis text-h5">
+      Tecnologias
+      <div class="fill-width d-flex flex-wrap text-subtitle-1">
+        <div v-for="tech in techs" :key="tech.name"
+          class="bg-background3 d-flex align-center justify-center mr-3 mt-2 px-2 rounded">
+          <v-avatar size="0.7rem" class=" mr-1" :id="tech.color">
+          </v-avatar> {{ tech.name }}
         </div>
-        <div class="fill-width d-flex flex-column text-medium-emphasis text-h5">
-            Formações
-            <div class="fill-width d-flex text-high-emphasis text-subtitle-1">
-                Nome da formação
-            </div>
-            <div class="fill-width d-flex text-high-emphasis text-subtitle-1">
-                <div>teste</div>
-                <div>teste</div>
-            </div>
+      </div>
+    </div>
+    <br>
+    <div class="fill-width d-flex flex-column text-medium-emphasis text-h5">
+      Formações
+      <div class="d-flex flex-wrap align-center">
+        <div v-for="inst in formacoes" :key="inst.name"
+          class="pr-6 mt-2 text-high-emphasis text-subtitle-1 align-center">
+          {{ inst.name }} <br>
+          {{ inst.inst }} <br>
+          {{ inst.description }}
         </div>
-        <div class="fill-width d-flex flex-column text-medium-emphasis text-h5">
-            Conexoes
-            <div class="fill-width d-flex text-high-emphasis text-subtitle-1">
-                <div>teste</div>
-                <div>teste</div>
-            </div>
-        </div>
-    </v-col>
+      </div>
+    </div>
+    <br>
+    <div class="fill-width d-flex flex-column text-medium-emphasis text-h5">
+      Conexoes
+      <div class="fill-width d-flex text-high-emphasis text-subtitle-1">
+        <ConectionsCard :items/>
+      </div>
+    </div>
+  </v-col>
 </template>
-
 <script setup>
-function createTechs(name, icon, color) {
-    return {
-        name: name,
-        icon: icon,
-        color: color
-    };
+import ConectionsCard from './ConectionsCard.vue';
+function createFormacao(inst, name, description) {
+  return {
+    inst: inst,
+    name: name,
+    description: description
+  };
+}
+let formacoes = []
+function createConections(icon, name, url) {
+  return {
+    icon: icon,
+    name: name,
+    url: url
+  };
+}
+let items = []
+items.push(createConections("mdi-github", "GitHub", "https://github.com/josermca01"))
+items.push(createConections("mdi-linkedin", "Linkedin", "https://www.linkedin.com/in/jose-ricardo-munhoz-456596217/"))
+items.push(createConections("mdi-gmail", "Gmail", "mailto:josermca01@gmail.com"))
+items.push(createConections("mdi-whatsapp", "WhatsApp", "https://api.whatsapp.com/send?phone=5515991511717"))
+formacoes.push(createFormacao("Facens", "Engenharia da Computação", "de 2019 a 2023"))
+function createTechs(name, color) {
+  return {
+    name: name,
+    color: color
+  };
 }
 let techs = []
-techs.push(createTechs("Java", "mdi-language-java", "java"))
-techs.push(createTechs("JavaScript", "mdi-language-javascript", "javascript"))
-techs.push(createTechs("Python", "mdi-language-python", "python"))
-techs.push(createTechs("TypeScript", "mdi-language-typescript", "typescript"))
-techs.push(createTechs("HTML", "mdi-language-html5", "html"))
-techs.push(createTechs("CSS", "mdi-language-css3", "css"))
+techs.push(createTechs("Java", "java"))
+techs.push(createTechs("JavaScript", "javascript"))
+techs.push(createTechs("Python", "python"))
+techs.push(createTechs("TypeScript", "typescript"))
+techs.push(createTechs("HTML", "html"))
+techs.push(createTechs("CSS", "css"))
 </script>
 <style scoped>
 #python {
-  background-image: conic-gradient(#3366ff 0deg,#3366ff 45deg,#ffff66 45deg,#ffff66 225deg,#3366ff 225deg);
+  background-image: conic-gradient(#3366ff 0deg, #3366ff 45deg, #ffff66 45deg, #ffff66 225deg, #3366ff 225deg);
 }
+
 #java {
-  background-image: conic-gradient(#ff8000 0deg,#ff8000 90deg, #4d79ff 90deg,#4d79ff 270deg,#ff8000 270deg);
+  background-image: conic-gradient(#ff8000 0deg, #ff8000 90deg, #4d79ff 90deg, #4d79ff 270deg, #ff8000 270deg);
 }
+
 #javascript {
-  background-color:  #ffcc00;
+  background-color: #ffcc00;
 }
+
 #typescript {
   background-color: #4d79ff;
 }
+
 #html {
   background-color: #ff8533;
 }
+
 #css {
-  background-color:#0066ff;
+  background-color: #0066ff;
 }
 </style>
---
+++ src/components/ConectionsCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-row class="ma-6 fill-height no-gutters" justify="center">
+        <template v-for="item in items" :key="item.name">
+            <v-col cols="12" sm="4" md="3">
+                <a :href="item.url" target="_blank" rel="noopener noreferrer" class="btn">
+                    <div class="bg-grey-darken-4 rounded h-100 w-100 pa-2 d-flex align-center overflow-hidden">
+                        <v-icon class="rounded-circle pa-6 align-centar":icon="item.icon"></v-icon>{{ item.name }}
+                    </div>
+                </a>
+            </v-col>
+        </template>
+    </v-row>
+</template>
+<script>
+export default {
+    props: {
+    items: { type:Object }
+  }
+}
+</script>
+<script setup>
+import { computed } from 'vue'
+import { useDisplay } from 'vuetify'
+const { name } = useDisplay()
+const colsNums = computed(() => {
+    // name is reactive and
+    // must use .value
+    switch (name.value) {
+        case 'xs': return 12
+        case 'sm': return 12
+        case 'md': return 6
+        case 'lg': return 6
+        case 'xl': return 4
+    }
+    return undefined
+})
+const projectsTechs = computed(() => {
+    // name is reactive and
+    // must use .value
+    switch (name.value) {
+        case 'xs': return "d-flex flex-column"
+        case 'sm': return "d-flex flex-column"
+        case 'md': return "d-flex flex-row-reverse md-flex-column"
+        case 'lg': return "d-flex flex-row-reverse md-flex-column"
+        case 'xl': return "d-flex flex-row-reverse md-flex-column"
+    }
+    return undefined
+})
+</script>
